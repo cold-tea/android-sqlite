246b6e448305cf5519571511fde7fa3665023482
Sandesh <sandesh.pokhrel56@gmail.com>
2017-12-10 22:55:25 +0545

    Second changes committed successfully.

--- app/src/main/java/com/heavenlyhell/sqlitedemo/SharedPrefActivity.java
+++ app/src/main/java/com/heavenlyhell/sqlitedemo/SharedPrefActivity.java
@@ -7,10 +7,26 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class SharedPrefActivity extends AppCompatActivity {
 
-    EditText etName;
-    TextView tvSpMessage;
+    EditText etName, etFirstName, etLastName;
+    TextView tvSpMessage, tvResult;
+
+    List<Person> persons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,10 @@ public class SharedPrefActivity extends AppCompatActivity {
 
         etName = findViewById(R.id.etName);
         tvSpMessage = findViewById(R.id.tvSpMessage);
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        tvResult = findViewById(R.id.tvResult);
+        persons = new ArrayList<>();
 
         SharedPreferences sharedPreferences = getSharedPreferences("sharedPref", MODE_PRIVATE);
         String tvResult = sharedPreferences.getString("name", "Default");
@@ -34,5 +54,78 @@ public class SharedPrefActivity extends AppCompatActivity {
         editor.putString("name", etName.getText().toString());
         editor.commit();
         tvSpMessage.setText(etName.getText().toString());
+    }
+
+    public void btnFileClick(View view) {
+        String firstName = etFirstName.getText().toString();
+        String lastName = etLastName.getText().toString();
+
+        if (firstName.length() == 0) {
+            etFirstName.setError("Provide First Name !");
+            return;
+        } else if (lastName.length() == 0) {
+            etLastName.setError("Provide Last Name !");
+            return;
+        }
+
+        Person person = new Person(firstName, lastName);
+        persons.add(person);
+
+        setToTextView(persons);
+    }
+
+    public void setToTextView(List<Person> persons) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Person person : persons) {
+            stringBuilder.append(person.getFirstName() + " " + person.getLastName() + "\n");
+        }
+        tvResult.setText(stringBuilder);
+        clearBoth();
+
+    }
+
+    public void writeToTheFile() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Person person : persons) {
+            stringBuilder.append(person.getFirstName() + "," + person.getLastName() + "\n");
+        }
+
+        try {
+            FileOutputStream fos = openFileOutput("file.txt", MODE_PRIVATE);
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fos));
+            bufferedWriter.write(stringBuilder.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void clearBoth() {
+        etFirstName.setText("");
+        etLastName.setText("");
+    }
+
+    public void btnSaveClick(View view) {
+        writeToTheFile();
+    }
+
+    public void btnFileLoad(View view) {
+        persons.clear();
+        try {
+            FileInputStream fis = openFileInput("file.txt");
+            String line;
+            Person person;
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis));
+            while((line = bufferedReader.readLine()) != null) {
+                StringTokenizer tokenizer = new StringTokenizer(line, ",");
+                person = new Person(tokenizer.nextToken(), tokenizer.nextToken());
+
+                persons.add(person);
+            }
+            setToTextView(persons);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
